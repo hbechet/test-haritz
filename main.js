bbe33ff1d364d37b4ffff5040d350638d7c2e107
haritz.bechet <haritz.bechet@gmail.com>
2024-06-17 11:33:09 +0200

    Un comentario de los cambios realizados

---
+++ main.js
@@ -0,0 +1,19 @@
+var name = "Haritz";
+let surname = "Bechet";
+const nick = "salchi";
+
+console.log(name);
+console.log(surname);
+console.log(nick);
+
+
+let piramide = '';
+
+for (var i = '1'; i < 10; i++) {
+    for (var x = 0; x < i; x++){
+        piramide += i;
+    }
+    piramide += '\n';
+}
+  
+  console.log(piramide);
